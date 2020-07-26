938bf3b17264c180abb39cae88e03c80c6a30d23
Arjun Somvanshi <arjunsomvanshi@gmail.com>
2020-07-26 17:22:19 +0530

    GUI screens and skeleton

---
+++ GUI.py
@@ -0,0 +1,20 @@
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.screenmanager import ScreenManager, Screen, SlideTransition, FadeTransition
+
+class Login(Screen):
+	def authenticate(self):
+		self.manager.transition=FadeTransition(duration=1.2)
+		self.manager.current = 'main'
+class MainWindow(Screen):
+	pass
+class Screen_Manager(ScreenManager):
+	pass
+kv=Builder.load_file("Chat.kv")
+class ChatApp(App):
+    def build(self):
+    	return kv
+
+if __name__ == '__main__':
+    ChatApp().run()
