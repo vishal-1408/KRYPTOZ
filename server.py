0dbf0c0426672495a085c8c3b86885b3289ef644
Vishal Reddy Chintham <54095062+vishal-1408@users.noreply.github.com>
2020-07-27 20:52:03 +0530

    Updates server.py
    Added extra functionalities

--- server.py
+++ server.py
@@ -3,60 +3,162 @@ from threading import Thread
 import socket
 
 
-
 def accept():
-    i=0
-    while 1:                                                             #threads are just created for making blocks of code execute parallely! like accepting the connections and handling clients all these work in parallel
-        client_socket,client_addr = server_socket.accept()
+    while 1:
+        client_socket,client_addr = server_socket.accept()        #threads are just created for making blocks of code execute parallely! like accepting the connections and handling clients all these work in parallel
         print("A client has been connected to the server")
-        address[client_socket]=client_addr
-        client_socket.sendall("Please enter your name and click enter".encode("ASCII"))
+        client_socket.send("Please enter your name and click enter".encode("ASCII"))
         message = client_socket.recv(1024)
         message=message.decode('ASCII')
-        client_name[client_socket]=message
-        Thread(target=handling_the_client,args=(client_socket,)).start()
-        print(len(client_name))
-        i+=1
+        clientinfo[client_socket]=[message]
+        d=1
+        while(d):
+           client_socket.send("Type 1 for creating a group and 2 for joinign a group and click enter".encode("ASCII"))
+           c=client_socket.recv(1024).decode("ASCII")
+           if c=="1":
+               d=create(client_socket)
+           elif c=="2":
+               d=join(client_socket)
+
+def create(c):
+    print("create")
+    if(len(groupinfo)!=0):
+        m="The foll. group names are not available"
+        for x in groupinfo:
+            m+=(str(x)+"\n")
+        c.send(m.encode("ASCII"))
+    c.send("Enter the name for your group".encode("ASCII"))
+    name=c.recv(1024).decode("ASCII")
+    c.send("Enter a password for the group".encode("ASCII"))
+    p=c.recv(1024).decode("ASCII")
+    groupinfo[name]=[p,[c]]
+    clientinfo[c].append(name)
+    Thread(target=handling_the_client,args=(c,)).start()
+    return 0
+
+def join(c):
+    e=1
+    while e:
+        m="The foll. groups are available:"
+        for x in groupinfo:
+            m+=(str(x)+"\n")
+        m+="Please enter the group name, you want to join: "
+        c.send(m.encode("ASCII"))
+        name=c.recv(1024).decode("ASCII")
+        if name not in groupinfo:
+            m="Group doesn't exist, Enter 1 to try again and 0 to go to main menu: "
+            c.send(m.encode("ASCII"))
+            res=c.recv(1024).decode("ASCII")
+            if res=="1":
+                continue
+            elif res=="0":
+                return 1
+        else:
+            m="Enter the password of the group: "
+            c.send(m.encode("ASCII"))
+            p=c.recv(1024).decode("ASCII")
+            cpass=groupinfo[name][0]
+            if p==cpass:
+                if len(groupinfo[name][1])<2:
+                    m="Welcome to "+name
+                    c.send(m.encode("ASCII"))
+                    groupinfo[name][1].append(c)
+                    clientinfo[c].append(name)
+                    Thread(target=handling_the_client,args=(c,)).start()
+                    return 0
+
+                else:
+                  m="Group is full, Enter 1 to try again and 0 for the main menu"
+                  c.send(m.encode("ASCII"))
+                  res=c.recv(1024)
+                  if res=="0":
+                    return 1
+                  else:
+                    continue
+            elif p!=cpass:
+                m="Entered password is incorrect,Enter 1 to try again and 0 for the main menu"
+                c.send(m.encode("ASCII"))
+                res=c.recv(1024).decode("ASCII")
+                if res=="0":
+                    return 1
+                else:
+                    continue
+
+    
         
 
 def handling_the_client(client):
-    message=client_name[client]+" has joined the chat app!"
+    message=clientinfo[client][0]+" has joined the room!"
     broadcast(message,"chatbot",client)
-    m="Welcome "+client_name[client]+"\nYou can enter a message and click enter and\nif you want to exit the app, please enter QUIT"
+    m="Welcome "+clientinfo[client][0]+"\nYou can enter a message and click enter and\nif you want to exit the app, please enter QUIT"
     client.send(m.encode("ASCII"))
     while 1:
         received=client.recv(1024)
         received=received.decode("ASCII")
-        print(received+" by "+client_name[client])
+        print(received+" by "+clientinfo[client][0])
         if not received=="QUIT":
-            broadcast(received,client_name[client])
+            broadcast(received,clientinfo[client][0],client)
         else:
-            print(client_name[client]+" has quit the app")
+            print(clientinfo[client][0]+" has quit the app")
+            if len(groupinfo[clientinfo[client][1]][1])==1:
+                 m="If you want to delete the group enter 1 else enter 0".encode("ASCII")
+                 client.send(m)
+                 res=clent.recv(1024)
+                 if res=="1":
+                    del groupinfo[clientinfo[client][1]][1]
+                 m="Bye! Hope you come back soon..."
+                 m=m.encode("ASCII")
+                 client.send(m)
+                 del clientinfo[client]
+                 client.close()
+                 break
+                 
             m="Bye! Hope you come back soon..."
             m=m.encode("ASCII")
             client.send(m)
-            name=client_name[client]
-            del client_name[client]
-            del address[client]
-            broadcast(name+" has left the room","ChatBot")
+            name=clientinfo[client][0]
+            groupn=clientinfo[client][1]
+            groupinfo[groupn][1].remove(client)
+            del clientinfo[client]
+            broadcast(name+" has left the room","chatBot","",groupn)
             client.close()
             break
         
-def broadcast(message,name="",client=""):
-    if client=="":
-        for x in client_name:
-            m=name+" : "+message
-            m=m.encode("ASCII")
-            x.send(m)
+def broadcast(message,name,client="",group=""):
+    if not client=="":
+        room=clientinfo[client][1]
+        clients=groupinfo[room][1]
+        if name=="chatbot":
+            for x in clients:
+                if not x==client:
+                    m=name+" : "+message
+                    m=m.encode("ASCII")
+                    x.send(m)
+        else:
+            for x in clients:
+                if not x==clients:
+                    m=name+" : "+message
+                    m=m.encode("ASCII")
+                    x.send(m)
     else:
-        for x in client_name:
-            if not x==client:
-                m=name+" : "+message
-                m=m.encode("ASCII")
-                x.send(m)
+        clients=groupinfo[group][1]
+        for x in clients:
+             m=name+" : "+message
+             m=m.encode("ASCII")
+             x.send(m)
+             
+        
+        
+        
+            
+            
+                
+            
 
-address = {}
-client_name = {}
+
+clientinfo={}
+groupinfo={}
+
 Host="127.0.0.1"
 port=3000
 server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
