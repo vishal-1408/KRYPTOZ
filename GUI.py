2e8b1e90f3a832e88ffd71c800be1c54d568c820
Arjun Somvanshi <arjunsomvanshi@gmail.com>
2020-08-17 08:10:06 +0530

    Screens Added

--- GUI.py
+++ GUI.py
@@ -7,10 +7,22 @@ class Login(Screen):
 	def authenticate(self):
 		self.manager.transition=FadeTransition(duration=1.2)
 		self.manager.current = 'main'
-class MainWindow(Screen):
+
+class JoinOrCreate(BoxLayout):
 	pass
+
+class CreateGroup(Screen):
+	pass
+
+class SelectGroup(Screen):
+	pass
+
+class ChatWindow(Screen):
+	pass
+
 class Screen_Manager(ScreenManager):
 	pass
+
 kv=Builder.load_file("Chat.kv")
 class ChatApp(App):
     def build(self):
