43d91a45a616b841b8ffa1066b6f947eb0533770
Arjun Somvanshi <arjunsomvanshi@gmail.com>
2020-10-16 17:37:13 +0530

    made changes to numeric checking of member limit

--- GUI.py
+++ GUI.py
@@ -264,12 +264,7 @@ class RecycleGroups(RecycleDataViewBehavior,BoxLayout):
 										)
 				self.success_auth.open()
 				global chamber_name_and_code
-<<<<<<< HEAD
-				chamber_name_and_code = self.design.chambername#Assigns group code to the global variable to use in next screen
-				print(chamber_name_and_code+ 'FROM GUI CHAMBER INFO')
-=======
 				chamber_name_and_code =self.design.chambername
->>>>>>> 44ca2f110a8b83e03bf15d4c71514af58e76b40c
 				auth_design.ids.okay.bind(on_release=self.transition)
 				self.refresh_members.cancel()
 				global refresh_group_list
